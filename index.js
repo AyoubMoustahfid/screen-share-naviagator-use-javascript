a4ae440a4318186fd6a512cac144e728fbb037be
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2021-04-08 10:07:20 +0100

    push version 0.0.1

---
+++ index.js
@@ -0,0 +1,12 @@
+const doc = document;
+const btn = doc.getElementById('button')
+const video = doc.getElementById('video')
+
+btn.addEventListener('click', () => {
+
+  navigator.mediaDevices.getDisplayMedia()
+    .then(media => {
+      video.srcObject = media
+    })
+
+})
